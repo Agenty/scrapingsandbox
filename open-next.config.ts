42757f4d989048a7356027ebbbb4a7c3568000bc
Vikash Rathee <vickyrathee@users.noreply.github.com>
2026-02-11 14:18:25 +0530

    ssg cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,9 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 
 export default defineCloudflareConfig({
+	incrementalCache: staticAssetsIncrementalCache,
+	enableCacheInterception: true,
 	// Uncomment to enable R2 cache,
 	// It should be imported as:
 	// `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
